189c3e319b5acecc6e870727b88c15632b396aab
Damian Odoemena <damucci05@yahoo.com>
2016-04-21 11:01:35 -0700

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  def index
+    friends = current_user.friend_ids
+    if friends.empty?
+      @users = User.where.not("id = #{current_user.id}")
+    else
+      @users = User.where.not("id IN (?) OR id = ?", friends, current_user.id)
+    end
+  end
+
+  def show
+  end
+
+  def update
+    @user = current_user
+    if !@user.update(description_params)
+      flash[:error] = "A problem occurred while updating your profile description, please try again later."
+    end
+    redirect_to dashboard_index_path
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def description_params
+    params.require(:user).permit(:description)
+  end
+end
